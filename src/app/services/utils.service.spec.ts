85f61f74946b0da230e93e75de22d4e3c41191a6
Sergio <s.albercaalberca@gmail.com>
2021-03-22 20:53:49 +0100

    add test for utilsService

--- src/app/services/utils.service.spec.ts
+++ src/app/services/utils.service.spec.ts
@@ -13,4 +13,13 @@ describe('UtilsService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should return a string', () => {
+    console.log(typeof service.generateRandomText() === 'string');
+    expect(typeof service.generateRandomText() === 'string').toBeTrue();
+  });
+
+  it('should contain the chain text Random', () => {
+    expect(service.generateRandomText().includes('Texto Random')).toBeTrue();
+  });
 });
